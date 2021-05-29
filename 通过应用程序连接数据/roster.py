f5bc507661b6968c7f8683018babc4b684a2278d
ztt-code <75900934+ztt-code@users.noreply.github.com>
2021-05-29 23:29:32 +0800

    Update roster.py

--- 通过应用程序连接数据/roster.py
+++ 通过应用程序连接数据/roster.py
@@ -6,12 +6,13 @@ import sys
 def fun(house_name):
     db = sqlite3.connect('students.db')
     cursor = db.cursor()
-
+    # 执行查询语句
     cursor.execute("select first,middle,last,birth from students where house = (?) order by last,first",[house_name])
     res = cursor.fetchall()
     if len(res) == 0:
         print("not in the house list, please input the right house name!")
         return 0
+    # 逐行打印
     for row in res:
         if row[1] == 'NULL':
             print(row[0],row[2],', born',row[-1])
